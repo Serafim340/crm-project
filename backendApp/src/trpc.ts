efc44ca8e3f678d63ebbf9ca4fd6be59cdcd211e
Serafim340 <127326173+Serafim340@users.noreply.github.com>
2024-12-24 18:53:05 +0800

    Added loadash & zod for content generation and validation

--- backendApp/src/trpc.ts
+++ backendApp/src/trpc.ts
@@ -1,15 +1,30 @@
 import { initTRPC } from '@trpc/server'
+import _ from 'lodash'
+import { z } from 'zod'
 
-const locations = [
-  { id: 'first', name: 'Участок 1', description: 'Описание участка 1' },
-  { id: 'second', name: 'Участок 2', description: 'Описание участка 2' },
-]
+const locations = _.times(100, (i) => ({
+  id: `location-${i}`,
+  name: `Участок ${i}`,
+  description: `Описание участка ${i}`,
+  text: _.times(100, (j) => `<p>Текст участка ${j} предложения для участка ${i}</p>`).join(''),
+}))
+
 const trpc = initTRPC.create()
 
 export const trpcRouter = trpc.router({
   getLocations: trpc.procedure.query(() => {
-    return { locations }
+    return { locations: locations.map((location) => _.pick(location, ['id', 'name', 'description'])) }
   }),
+  getLocation: trpc.procedure
+    .input(
+      z.object({
+        locationId: z.string(),
+      })
+    )
+    .query(({ input }) => {
+      const location = locations.find((location) => location.id === input.locationId)
+      return { location: location || null }
+    }),
 })
 
 export type TrpcRouter = typeof trpcRouter
--- frontendApp/src/pages/ViewLocationPage/index.tsx
+++ frontendApp/src/pages/ViewLocationPage/index.tsx
@@ -1,16 +1,31 @@
 import { useParams } from 'react-router-dom'
+import { type ViewIdeaRouteParams } from '../../lib/routes'
+import { trpc } from '../../lib/trpc'
 
 export const ViewLocationPage = () => {
-  const { locationId } = useParams() as { locationId: string }
+  const { locationId } = useParams() as ViewIdeaRouteParams
+
+  const { data, error, isLoading, isFetching, isError } = trpc.getLocation.useQuery({
+    locationId,
+  })
+
+  if (isLoading || isFetching) {
+    return <span>Загрузка..</span>
+  }
+
+  if (isError) {
+    return <span>Ошибка: {error.message}</span>
+  }
+
+  if (!data.location) {
+    return <span>Участок не найден</span>
+  }
+
   return (
     <div>
-      <h1>{locationId}</h1>
-      <p>Описание участка №1</p>
-      <div>
-        <p>Наличие</p>
-        <p>Доступность</p>
-        <p>Стоимость</p>
-      </div>
+      <h1>{data.location.name}</h1>
+      <p>{data.location.description}</p>
+      <div dangerouslySetInnerHTML={{ __html: data.location.text }} />
     </div>
   )
 }
